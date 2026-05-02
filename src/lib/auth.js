0fae8d8167b44a935cb646894cd725ed29652222
saikot05 <saikotislam08@gmail.com>
2026-05-02 22:16:32 +0600

    fix auth

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,21 +1,17 @@
-import dns from "node:dns";
-dns.setServers(['8.8.8.8', '8.8.4.4'])
-import { betterAuth } from "better-auth";
-import { MongoClient } from "mongodb";
-import { mongodbAdapter } from "better-auth/adapters/mongodb";
-
-const client = new MongoClient(process.env.MONGO_URI);
-const db = client.db('suncart');
-
 export const auth = betterAuth({
-    database: mongodbAdapter(db, {
-        client
-    }),
+    database: mongodbAdapter(db, { client }),
     trustedOrigins: [
         "http://localhost:3000",
         "https://sun-cart-8l8y.vercel.app",
         "https://sun-cart-8l8y-git-main-saikot05s-projects.vercel.app",
+        "https://sun-cart-8l8y-ljkx0xfwn-saikot05s-projects.vercel.app",
     ],
+    advanced: {
+        crossSubDomainCookies: {
+            enabled: true,
+            domain: "vercel.app"
+        }
+    },
     emailAndPassword: {
         enabled: true,
     },
