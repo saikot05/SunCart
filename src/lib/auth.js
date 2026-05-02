ebfdb5ace3a10ca8396940feb5417bd67f8cb52c
saikot05 <saikotislam08@gmail.com>
2026-05-02 23:40:59 +0600

    email and google login check

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -8,19 +8,11 @@ const db = client.db('suncart');
 export const auth = betterAuth({
     database: mongodbAdapter(db, { client }),
 
-    baseURL: process.env.BETTER_AUTH_URL,
     trustedOrigins: [
         "http://localhost:3000",
         "https://sun-cart-8l8y.vercel.app",
-        "https://*.vercel.app",
-    ],
 
-    advanced: {
-        disableCSRFCheck: true,
-        crossSubDomainCookies: {
-            enabled: false,
-        }
-    },
+    ],
 
     emailAndPassword: {
         enabled: true,
