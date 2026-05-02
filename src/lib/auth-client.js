bc05880506ee0a79a84fa054058fe13a4d034670
saikot05 <saikotislam08@gmail.com>
2026-05-02 20:07:16 +0600

    auth client baseurl fix

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,8 +1,7 @@
 import { createAuthClient } from "better-auth/react"
-export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: process.env.NEXT_PUBLIC_AUTH_BASE_URL
-})
 
+export const authClient = createAuthClient({
+    baseURL: process.env.NEXT_PUBLIC_AUTH_BASE_URL || "https://sun-cart-8l8y.vercel.app"
+})
 
 export const { signIn, signUp, useSession } = createAuthClient()
