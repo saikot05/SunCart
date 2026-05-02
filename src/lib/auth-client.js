55f71d06421daa94709ee2e905dfc4085bf9755e
saikot05 <saikotislam08@gmail.com>
2026-05-02 21:29:28 +0600

    fix google authentication

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 
 export const authClient = createAuthClient({
-    baseURL: process.env.NEXT_PUBLIC_AUTH_BASE_URL || "https://sun-cart-8l8y.vercel.app"
+    baseURL: process.env.NEXT_PUBLIC_APP_URL || "https://sun-cart-8l8y.vercel.app"
 })
 
-export const { signIn, signUp, useSession } = createAuthClient()
+export const { signIn, signUp, useSession } = authClient()
