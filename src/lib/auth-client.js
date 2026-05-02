e4728fac013ff9ad23c17c9637f5a25c5ff7edbb
saikot05 <saikotislam08@gmail.com>
2026-05-02 21:53:32 +0600

    solve the authentication problem

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 
 export const authClient = createAuthClient({
-    baseURL: process.env.NEXT_PUBLIC_APP_URL || "https://sun-cart-8l8y.vercel.app"
+    baseURL: "https://sun-cart-8l8y.vercel.app"
 })
 
-export const { signIn, signUp, useSession } = authClient()
+export const { signIn, signUp, useSession } = authClient
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,7 +1,6 @@
 import dns from "node:dns";
 dns.setServers(['8.8.8.8', '8.8.4.4'])
-import { betterAuth }
-from "better-auth";
+import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
@@ -10,13 +9,12 @@ const db = client.db('suncart');
 
 export const auth = betterAuth({
     database: mongodbAdapter(db, {
-        // Optional: if you don't provide a client, database transactions won't be enabled.
         client
     }),
     trustedOrigins: [
         "http://localhost:3000",
         "https://sun-cart-8l8y.vercel.app",
-
+        "https://sun-cart-8l8y-git-main-saikot05s-projects.vercel.app",
     ],
     emailAndPassword: {
         enabled: true,
